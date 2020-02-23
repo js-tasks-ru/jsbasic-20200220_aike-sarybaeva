c0f4fff81851235e154ea20a4f8cef2221e62f6c
aikelovely <34231522+aikelovely@users.noreply.github.com>
2020-02-23 22:33:13 +0300

    Update index.js

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -9,7 +9,19 @@ function print(text) {
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
+
+// Необходимо, чтобы на сайте можно было поприветствовать только пользователей,
+//   которые удовлетворяют следующему условию – имя не пустое, без пробелов, минимум 4 символа.
+
 function isValid(name) {
+var success  = false;
+
+  if(name && name.split(' ').length-1 == 0 && name.length > 3) {
+  success = true;
+} else {
+    success = false;
+  }
+return success;
 }
 
 function sayHello() {
