c0b45df5851f6fda29bcfe2fcb69940d0c09f5ba
Aike <aikelovely@mail.ru>
2020-03-04 22:55:07 +0300

    sumObjValues

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,20 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
 
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+  let sum = 0;
+
+  return parseInt(Object.values(salaries).reduce((a, b) => a + b, 0));
+}
+
+console.log(sumSalary(salaries));
